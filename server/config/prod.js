be26932006710ec74bf5fde375cd42d21d39771e
Patrick Rivera <pxr13@my.fsu.edu>
2018-05-15 21:55:40 -0400

    Added prod redirectURI and frontend domain

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,5 +3,7 @@ module.exports = {
     process.env.PROD_MONGO_URI_UN_AND_PW
   )}${process.env.PROD_MONGO_URI_END}`,
   spotifyClientId: process.env.PROD_SPOTIFY_CLIENT_ID,
-  spotifyClientSecret: process.env.PROD_SPOTIFY_CLIENT_SECRET
+  spotifyClientSecret: process.env.PROD_SPOTIFY_CLIENT_SECRET,
+  redirectUri: process.env.PROD_REDIRECT_URI,
+  frontendDomain: process.env.PROD_FRONTEND_DOMAIN
 };
