1d192fce4e01862223721a4fb1f70209ee0bf377
Patrick Rivera <pxr13@my.fsu.edu>
2018-05-15 23:10:29 -0400

    updated prod mongoUri

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mongoURI: `${process.env.PROD_MONGO_URI_ROOT}${encodeURIComponent(
-    process.env.PROD_MONGO_URI_UN_AND_PW
-  )}${process.env.PROD_MONGO_URI_END}`,
+  mongoURI: process.env.PROD_MONGO_URI,
   spotifyClientId: process.env.PROD_SPOTIFY_CLIENT_ID,
   spotifyClientSecret: process.env.PROD_SPOTIFY_CLIENT_SECRET,
   redirectUri: process.env.PROD_REDIRECT_URI,
