7db21fbeb87ba3374af1bc31051b5a98eee38db1
Patrick Rivera <pxr13@my.fsu.edu>
2018-05-19 14:55:51 -0400

    Added cache routes and tests

--- server/controllers/playlist/cache.js
+++ server/controllers/playlist/cache.js
@@ -1,8 +1,32 @@
+const curry = require('ramda/src/curry');
+const pipe = require('ramda/src/pipe');
+
 const code = require('../../utils/statusCodes');
 const User = require('../../models/User');
 
-const isEmpty = (playlists) => !playlists.length;
+const formatPlaylist = curry((query, tracks) => ({
+  query,
+  tracks: [...tracks]
+}));
+
+const pushToCache = curry((targetUser, formattedPlaylist) => {
+  targetUser.cache.push({ ...formattedPlaylist });
+  return targetUser;
+});
+
+const save = async (targetUser) => {
+  await targetUser.save();
+};
+
+const saveToCache = async (targetUser, query, tracks) =>
+  pipe(formatPlaylist(query), pushToCache(targetUser), save)(tracks);
 
 module.exports = async (req, res, next) => {
-  res.send({});
+  const { spotifyId, tracks, query } = req.body;
+
+  const targetUser = await User.findOne({ spotifyId });
+
+  await saveToCache(targetUser, query, tracks);
+
+  res.send({ success: true });
 };
--- server/test/routes/cachePlaylist.test.js
+++ server/test/routes/cachePlaylist.test.js
@@ -10,31 +10,33 @@ const keys = require('../../config/keys');
 chai.use(chaiHttp);
 const expect = chai.expect;
 
-describe('POST /api/playlist/save', () => {
-  it('should save a playlist', async () => {
-    const route = '/api/playlist/save';
-    const data = {
+describe('POST /api/playlist/cache', () => {
+  it.only('should cache a playlist', async () => {
+    const route = '/api/playlist/cache';
+    const playlistData = {
+      query: 'programming',
       spotifyId: 123,
-      title: 'My Awesome Playlist',
       tracks: [{ name: 'Heller' }, { name: 'Ayo' }]
     };
 
-    let user = new User({ spotifyId: 123, playlists: [] });
+    let user = new User({ spotifyId: 123, cache: [] });
     await user.save();
 
     user = await User.findOne({ spotifyId: 123 });
-    const oldPlaylistCount = user.playlists.length;
+    const oldPlaylistCount = user.cache.length;
 
     const res = await chai
       .request(app)
       .post(route)
-      .send(data);
+      .send(playlistData);
 
     user = await User.findOne({ spotifyId: 123 });
-    const newPlaylistCount = user.playlists.length;
+    const newPlaylistCount = user.cache.length;
 
     expect(res).to.have.status(code.OK);
     expect(res.body.success).to.be.true;
+    expect(user.cache[0].tracks[0].name).to.equal('Heller');
+    expect(user.cache[0].tracks[1].name).to.equal('Ayo');
     expect(newPlaylistCount).to.equal(oldPlaylistCount + 1);
   });
 });
